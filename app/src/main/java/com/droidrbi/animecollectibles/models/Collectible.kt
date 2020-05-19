936dc70113a5ccfaa57447ea6c6dc9724af0c95c
ipshitaRB <ipshitarburman@gmail.com>
2020-05-19 16:12:01 -0400

    Add cost to model

--- app/src/main/java/com/droidrbi/animecollectibles/models/Collectible.kt
+++ app/src/main/java/com/droidrbi/animecollectibles/models/Collectible.kt
@@ -1,4 +1,7 @@
 package com.droidrbi.animecollectibles.models
 
-data class Collectible(val thumbnail: Int = 0, val character: String = "", val collection: String = "") {
+data class Collectible(val thumbnail: Int = 0,
+                       val character: String = "",
+                       val collection: String = "",
+                       val cost: String = "") {
 }
