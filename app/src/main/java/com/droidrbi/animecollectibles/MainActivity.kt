f68c3a12a5244de3fd2498f604277ebcaca01077
ipshitaRB <ipshitarburman@gmail.com>
2020-05-19 16:52:18 -0400

    Add recyclerView and adapter

--- app/src/main/java/com/droidrbi/animecollectibles/MainActivity.kt
+++ app/src/main/java/com/droidrbi/animecollectibles/MainActivity.kt
@@ -2,11 +2,23 @@ package com.droidrbi.animecollectibles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.droidrbi.animecollectibles.models.Collectible
+import com.droidrbi.animecollectibles.utils.DatasetGenerator
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var _dataset: ArrayList<Collectible>
+    private lateinit var _adapter: CollectibleListAdapter
+    private lateinit var _recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        _recyclerView = findViewById(R.id.collectibleListView)
+        _dataset = DatasetGenerator.generateData()
+        _adapter = CollectibleListAdapter(_dataset)
     }
 }
