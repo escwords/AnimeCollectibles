b49ecffde3be61636fd20af01f35a2bd9b43a8ec
ipshitaRB <ipshitarburman@gmail.com>
2020-05-19 16:29:01 -0400

    Create DatasetGenerator

---
+++ app/src/main/java/com/droidrbi/animecollectibles/utils/DatasetGenerator.kt
@@ -0,0 +1,15 @@
+package com.droidrbi.animecollectibles.utils
+
+import com.droidrbi.animecollectibles.models.Collectible
+
+class DatasetGenerator {
+
+    companion object Factory {
+        fun generateData(): ArrayList<Collectible> {
+            val result = ArrayList<Collectible>()
+
+
+            return result
+        }
+    }
+}
