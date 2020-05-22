389415104337a0d66e30ec63bd5b11d96f5a61bf
ipshitaRB <ipshitarburman@gmail.com>
2020-05-22 17:28:27 -0400

    Show product detail

--- app/src/main/java/com/droidrbi/animecollectibles/AboutActivity.kt
+++ app/src/main/java/com/droidrbi/animecollectibles/AboutActivity.kt
@@ -3,7 +3,9 @@ package com.droidrbi.animecollectibles
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import com.droidrbi.animecollectibles.models.Collectible
 
 class AboutActivity : AppCompatActivity() {
 
@@ -11,6 +13,7 @@ class AboutActivity : AppCompatActivity() {
     lateinit var collectionTextView: TextView
     lateinit var costTextView: TextView
     lateinit var addToCartButton: Button
+    lateinit var thumbnail: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +23,36 @@ class AboutActivity : AppCompatActivity() {
         collectionTextView = findViewById(R.id.collectionTextView)
         costTextView = findViewById(R.id.priceTextView)
         addToCartButton = findViewById(R.id.addToCartButton)
+        thumbnail = findViewById(R.id.thumbnailImageView)
+
+        val collectible = retrieveCollectible()
+
+        updateUI(collectible)
 
 
+    }
+
+    private fun updateUI(collectible: Collectible) {
+
+        if (collectible.thumbnail != 0){
+            thumbnail.setImageResource(collectible.thumbnail)
+        }
+        if(collectible.cost == "SOLD OUT"){
+            addToCartButton.text = getString(R.string.join_waitlist)
+        }
+        costTextView.text = collectible.cost
+        characterTextView.text = collectible.character
+        collectionTextView.text = collectible.collection
+
+    }
+
+    private fun retrieveCollectible(): Collectible {
+        val intent = getIntent()
+        val character  = intent.getStringExtra("character")
+        val collection = intent.getStringExtra("collection")
+        val cost = intent.getStringExtra("cost")
+        val thumbnail = intent.getIntExtra("thumbnail", 0)
+
+        return Collectible(thumbnail, character!!, collection!!, cost!!)
     }
 }
