cae35aff5d5ffb7af3c2555ef24cbd1f5e6ca475
ipshitaRB <ipshitarburman@gmail.com>
2020-05-22 17:07:35 -0400

    Instantiate views

--- app/src/main/java/com/droidrbi/animecollectibles/AboutActivity.kt
+++ app/src/main/java/com/droidrbi/animecollectibles/AboutActivity.kt
@@ -2,11 +2,25 @@ package com.droidrbi.animecollectibles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class AboutActivity : AppCompatActivity() {
+
+    lateinit var characterTextView: TextView
+    lateinit var collectionTextView: TextView
+    lateinit var costTextView: TextView
+    lateinit var addToCartButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        characterTextView = findViewById(R.id.characterTextView)
+        collectionTextView = findViewById(R.id.collectionTextView)
+        costTextView = findViewById(R.id.priceTextView)
+        addToCartButton = findViewById(R.id.addToCartButton)
+
+
     }
 }
