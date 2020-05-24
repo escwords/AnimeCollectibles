7a0f8a2b5e35f420a048403bb8adab160f0cc0fe
ipshitaRB <ipshitarburman@gmail.com>
2020-05-23 20:51:42 -0400

    Bind Item View

--- app/src/main/java/com/droidrbi/animecollectibles/CollectibleListAdapter.kt
+++ app/src/main/java/com/droidrbi/animecollectibles/CollectibleListAdapter.kt
@@ -1,44 +1,38 @@
 package com.droidrbi.animecollectibles
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.droidrbi.animecollectibles.databinding.ItemLayoutBinding
 import com.droidrbi.animecollectibles.models.Collectible
 
-class CollectibleListAdapter(private val _dataset:ArrayList<Collectible>):
+class CollectibleListAdapter(private val _dataset: ArrayList<Collectible>) :
     RecyclerView.Adapter<CollectibleListAdapter.CollectibleViewHolder>() {
 
-    class CollectibleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        var characterTextView: TextView
-        var thumbnailImageView: ImageView
-        var costTextView: TextView
-        init {
-            characterTextView =itemView.findViewById(R.id.characterTextView)
-            thumbnailImageView = itemView.findViewById(R.id.thumbnail)
-            costTextView = itemView.findViewById(R.id.costTextView)
+    class CollectibleViewHolder(_itemLayoutBinding: ItemLayoutBinding) :
+        RecyclerView.ViewHolder(_itemLayoutBinding.root) {
 
-        }
-
+        var itemLayoutBinding: ItemLayoutBinding = _itemLayoutBinding
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CollectibleViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_layout, parent, false)
+        val itemLayoutBinding = DataBindingUtil.inflate<ItemLayoutBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.item_layout,
+            parent,
+            false
+        )
 
-        return CollectibleViewHolder(itemView)
+        return CollectibleViewHolder(itemLayoutBinding)
     }
 
     override fun getItemCount(): Int = _dataset.size
 
     override fun onBindViewHolder(holder: CollectibleViewHolder, position: Int) {
         val item = _dataset[position]
-        holder.characterTextView.text = item.character
-        holder.costTextView.text = item.cost
-        if (item.thumbnail != 0) {
-            holder.thumbnailImageView.setImageResource(item.thumbnail)
-        }
+        holder.itemLayoutBinding.collectible = item
+        holder.itemLayoutBinding.thumbnail.setImageResource(item.thumbnail)
+
     }
 }
--- app/src/main/java/com/droidrbi/animecollectibles/MainActivity.kt
+++ app/src/main/java/com/droidrbi/animecollectibles/MainActivity.kt
@@ -2,28 +2,29 @@ package com.droidrbi.animecollectibles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.droidrbi.animecollectibles.databinding.ActivityMainBinding
 import com.droidrbi.animecollectibles.models.Collectible
 import com.droidrbi.animecollectibles.utils.DatasetGenerator
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var _mainBinding: ActivityMainBinding
     private lateinit var _dataset: ArrayList<Collectible>
     private lateinit var _adapter: CollectibleListAdapter
-    private lateinit var _recyclerView: RecyclerView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        _recyclerView = findViewById(R.id.collectibleListView)
+        _mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        _mainBinding.collectibleListView.layoutManager = LinearLayoutManager(this)
+        _mainBinding.collectibleListView.hasFixedSize()
         _dataset = DatasetGenerator.generateData()
         _adapter = CollectibleListAdapter(_dataset)
-        val layoutManager = LinearLayoutManager(applicationContext)
-        _recyclerView.layoutManager = layoutManager
-        _recyclerView.adapter = _adapter
-        _adapter.notifyDataSetChanged()
+        _mainBinding.collectibleListView.adapter = _adapter
+        _mainBinding.collectibleListView.adapter
+
     }
 }
