8ae9f6ae4f2d98af58edf3ef0b33b8ebb19b839f
ipshitaRB <ipshitarburman@gmail.com>
2020-05-25 19:45:11 -0400

    Bind data to Detail layout

--- app/src/main/java/com/droidrbi/animecollectibles/DetailFragment.kt
+++ app/src/main/java/com/droidrbi/animecollectibles/DetailFragment.kt
@@ -1,24 +1,37 @@
 package com.droidrbi.animecollectibles
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import com.droidrbi.animecollectibles.R
+import androidx.fragment.app.Fragment
+import com.droidrbi.animecollectibles.databinding.FragmentDetailBinding
+import com.droidrbi.animecollectibles.models.Collectible
 
 /**
  * A simple [Fragment] subclass.
  */
 class DetailFragment : Fragment() {
 
+    private lateinit var _detailBinding: FragmentDetailBinding
+    private lateinit var _collectible: Collectible
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _detailBinding = FragmentDetailBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        _collectible = Collectible(
+            R.drawable.mikasa,
+            "Mikasa Ackerman",
+            "Attack on Titan",
+            "SOLD OUT"
+        )
+
+        _detailBinding.collectible = _collectible
+        return _detailBinding.root
     }
+
 
 }
