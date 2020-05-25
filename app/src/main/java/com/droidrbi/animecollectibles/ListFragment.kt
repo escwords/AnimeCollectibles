cfaf89affce0fa329f014872b8c2cfd6f74fe987
Ipshita Roy Burman <ipshitarburman@gmail.com>
2020-05-25 11:51:08 -0400

    Transfer UI elements from Activity

--- app/src/main/java/com/droidrbi/animecollectibles/ListFragment.kt
+++ app/src/main/java/com/droidrbi/animecollectibles/ListFragment.kt
@@ -1,21 +1,33 @@
 package com.droidrbi.animecollectibles
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.droidrbi.animecollectibles.databinding.FragmentListBinding
+import com.droidrbi.animecollectibles.models.Collectible
 
 /**
  * A simple [Fragment] subclass.
  */
 class ListFragment : Fragment() {
 
+    private lateinit var _mainBinding: FragmentListBinding
+    private lateinit var _dataset: ArrayList<Collectible>
+    private lateinit var _adapter: CollectibleListAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+/*        _mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        _mainBinding.collectibleListView.layoutManager = LinearLayoutManager(this)
+        _mainBinding.collectibleListView.hasFixedSize()
+        _dataset = DatasetGenerator.generateData()
+        _adapter = CollectibleListAdapter(_dataset)
+        _mainBinding.collectibleListView.adapter = _adapter
+        _mainBinding.collectibleListView.adapter*/
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
