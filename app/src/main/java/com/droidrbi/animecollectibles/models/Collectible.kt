cd79d3a4d8bf4ca7af1e8acd86e7c1230e9ab267
ipshitaRB <ipshitarburman@gmail.com>
2020-05-25 21:11:56 -0400

    Make model parcelable

--- app/src/main/java/com/droidrbi/animecollectibles/models/Collectible.kt
+++ app/src/main/java/com/droidrbi/animecollectibles/models/Collectible.kt
@@ -1,9 +1,42 @@
 package com.droidrbi.animecollectibles.models
 
-import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.annotation.Keep
 
-data class Collectible(val thumbnail: Int,
-                       val character: String,
-                       val collection: String,
-                       val cost: String) {
+@Keep
+
+data class Collectible(
+    val thumbnail: Int,
+    val character: String?,
+    val collection: String?,
+    val cost: String?
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(thumbnail)
+        parcel.writeString(character)
+        parcel.writeString(collection)
+        parcel.writeString(cost)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Collectible> {
+        override fun createFromParcel(parcel: Parcel): Collectible {
+            return Collectible(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Collectible?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
